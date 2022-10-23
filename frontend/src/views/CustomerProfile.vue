17f937bb3ac1d6e0048128a1bd7f5ed80b57865b
teoruixin <ruixin.teo.2020@scis.smu.edu.sg>
2022-10-23 16:50:05 +0800

    access cust profile

--- frontend/src/views/CustomerProfile.vue
+++ frontend/src/views/CustomerProfile.vue
@@ -7,21 +7,44 @@
   </template>
   
   <script>
-  export default {
-      name: "CustomerProfile",
-      data(){
-          return{
   
-          }
-      },
-      created(){
+    export default {
+        name: "CustomerProfile",
+        data(){
+            return{
+                details: [],
+            }
+        },
+        created(){
+            var headerObj = this.$store.state.headerObj
+            headerObj["serviceName"] = "getCustomerDetails"           
+            var header = JSON.stringify(headerObj); 
+
+            this.axios.post("http://tbankonline.com/SMUtBank_API/Gateway?Header="+header).then((response)=>{
+                var data = response.data.Content.ServiceResponse
+                var errorcode = data.ServiceRespHeader.GlobalErrorID
+                if(errorcode == "010000"){
+                    console.log(data.DepositoryList.Depository)
+                    this.details = data.DepositoryList.Depository
+                }
+                else{
+                    this.modalActive = true
+                    this.modalMessage = data.ServiceRespHeader.ErrorDetails
+                    this.success = false;
+                }
+
+            }).catch((error)=>{
+                this.modalActive = true
+                this.modalMessage = error
+            }).finally(()=>{
+                this.loading = false
+            })
+        },
+        methods:{
+            
+        }
   
-      },
-      methods:{
-          
-      }
-  
-  }
+    }
   </script>
   
   <style>
