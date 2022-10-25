a18fca7efca6172110fa5c8bd0986e32e458d0ea
kohpeiling <52946268+kohpeiling@users.noreply.github.com>
2022-10-25 15:13:40 +0800

    Edited font and added new logo

--- frontend/src/components/MainNavigation.vue
+++ frontend/src/components/MainNavigation.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/"><img src="../assets/logo.png"  height="50" width="100" /></a>
+        <a class="navbar-brand" href="/"><img src="../assets/Simplifi.png" height="40" width="50" /></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
